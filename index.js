3120dc244eafe632e573ff818182b3b7a17e03cd
ocampoad <ocampo.adriane13@gmail.com>
2022-06-22 16:46:39 -0700

    added questions and started prompts and generator

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the title of your project?",
-    "Description of your project?",
-    "Please inlcude instructions",
-    "Contribution guidelines",
-    "Test instructions",
+    {
+        message: "What is the title of your project?",
+        name: "title",
+        type: "input",
+    },
+    {
+        message: "Description of your project?",
+        name: "description",
+        type: "input",
+    },
+    {
+        message: "Please inlcude installation instructions",
+        name: "installation",
+        type: "input",
+    },
+    {
+        message: "Usage information",
+        name: "usage",
+        type: "input",
+    },
+    {
+        message: "Contribution guidelines",
+        name: "contributing",
+        type: "input",
+    },
+    {
+        message:  "Test instructions",
+        name: "tests",
+        type: "input",
+    },
 ];
+inquirer.prompt(questions).then(answers => {
+    writeToFile("someFileName",answers)
+})
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const markDown = generateMarkdown(data);
+    console.log(markDown)
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,24 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}\n
+  ## Table of Contents
+    1. [Description](#Description)
+    2. [Installation](#Installation)
+    3. [Usage](#Usage)
+    4. [Contributing](#Contributing)
+    5. [Tests](#Tests)
+  ## Description\n
+  ${data.description}\n
+  ## Installation\n
+  ${data.installation}\n
+  ## Usage\n
+  ${data.usage}\n
+  ## Contributing\n
+  ${data.contributing}\n
+  ## Tests\n
+  ${data.tests}\n
 `;
 }
 
