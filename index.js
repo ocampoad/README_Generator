979c9482ebd09c108d64b66074e7a166057e47d0
ocampoad <ocampo.adriane13@gmail.com>
2022-06-22 14:24:51 -0700

    added package.json

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "Description of your project?",
+    "Please inlcude instructions",
+    "Contribution guidelines",
+    "Test instructions",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
